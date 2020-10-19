4e220386a79d1abbcf82994041f52abb616615c8
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-10-19 19:55:51 +0100

    created var for name email text and subject inside javascript validation function for form

---
+++ script.js
@@ -0,0 +1,8 @@
+function validation(){
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+}
