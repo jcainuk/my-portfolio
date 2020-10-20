19af5bea17699790c78a20ed5878f9eb3c58cefe
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-10-20 12:22:27 +0100

    Started rebuilding Javascript function to alert name value when submit is clicked and fixed the form html element which should have said return functionname in speech marks after onsubmit

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
-document.getElementById('submit').addEventListener( 'click', function() {
-   alert('Button has been clicked');
-});
+
+
+function validation(){
+
+   let name = document.getElementById('name').value;
+   let email = document.getElementById('email');
+   let subject = document.getElementById('subject');
+   let message = document.getElementById('message');
+   
+   let error_message = document.getElementById("error_message");
+   
+   alert(name);
+}
+
+
